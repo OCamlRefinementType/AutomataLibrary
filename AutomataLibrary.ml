4a88fa4a9a25b917a8fe579685d39b2f063cd776
zhouzhezz <zorichzorich@gmail.com>
2025-05-07 10:22:36 -0700

    fix

--- AutomataLibrary.ml
+++ AutomataLibrary.ml
@@ -1,5 +1,12 @@
-include Fa
+include Sfa
 module QcFa = QcFa
 
 (* module Desymbolic = Desymbolic *)
 (* module Rawdesym = Rawdesym *)
+open Zutils
+open SFA
+
+let realize_sregex (checker : Nt.t sevent -> bool) (r : CharSet.t regex) =
+  let fa = compile_regex_to_dfa r in
+  let fa = minimize @@ dfa_realize checker fa in
+  dfa_to_reg fa
